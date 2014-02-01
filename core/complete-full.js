57e4e588b8c1808b5cfac2821831c7ae98e205b5
Skateside <sk85ide@hotmail.com>
2014-02-01 18:38:18 +0000

    Updating minified files and structure

--- core/complete-full.js
+++
@@ -1,7 +0,0 @@
-//@prepros-append "core.js"
-//@prepros-append "array.js"
-//@prepros-append "boolean.js"
-//@prepros-append "function.js"
-//@prepros-append "number.js"
-//@prepros-append "object.js"
-//@prepros-append "string.js"
